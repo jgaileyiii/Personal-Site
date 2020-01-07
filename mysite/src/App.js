811a75e378bd06a424f449de93939de2c7fb2896
Joe Gailey <jgaileyiii@gmail.com>
2020-01-07 13:00:52 -0700

    created landing page. Styled. Created Routes and Nav.

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './App.css';
+import Home from './components/Home'
+import About from './components/About'
+import Links from './components/Links'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render (){
+    return (
+      <div className="joes-site">
+        <Router>
+          <header>
+            <Link className="full-name" to='/home/'>Joseph Vance Gailey III</Link>
+          </header>
+            <div className="nav">
+              <Link className='link' to='/home/'>Home</Link>
+              <Link className='link' to='/about/'>About</Link>
+              <Link className='link' to='/links/'>Links</Link>
+            </div>
+            <Switch>
+              <Route path='/home/' component={Home}></Route>
+              <Route path='/about/' component={About}></Route>
+              <Route path='/links/' component={Links}></Route>
+            </Switch>
+          </Router>
+          <div className="custom-footer"></div>
+      </div>
+    );
+  }
 }
-
 export default App;
