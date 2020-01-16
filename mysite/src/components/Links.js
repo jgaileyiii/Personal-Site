f2f07455b78a8fb1c07efdd8bc7cdefb33118093
Joe Gailey <jgaileyiii@gmail.com>
2020-01-16 15:32:03 -0700

    added and styled links to prof. sites

--- mysite/src/components/Links.js
+++ mysite/src/components/Links.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../App.css'
+import './Links.css'
 
 export default function Links () {
     return (
-        <div></div>
+        <div className="links">
+            <a className="linked-in" href="https://www.linkedin.com/in/joe-gailey/"/>
+            <a className="github" href="https://github.com/jgaileyiii"/>
+            <a className="medium" href="https://medium.com/@jgaileyiii"/>
+        </div>
     )
 }
