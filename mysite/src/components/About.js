b00bbd2bd3075a9aa72a19980fa9835f1a416d9e
Joe Gailey <jgaileyiii@gmail.com>
2020-01-16 17:09:01 -0700

    worked on details of about section. finished link styles.

--- mysite/src/components/About.js
+++ mysite/src/components/About.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import '../App.css'
+import './About.css'
 
 export default function About () {
     return (
-        <div></div>
+        <div className='about'>
+            <h3 className="title">About Me</h3>
+            <div className='content'>
+                <p>Full stack web developer with experience in Ruby on Rails, Node.js, JavaScript, React, Vue. With a background in coffee roasting and warehouse management, I developed a strong ethic for teamwork, personal accountability, and producing an excellent product. With strong skills in communication, team cooperation, and a strong independent work ethic that help create a positive, productive and enjoyable work environment, I furthermore relish in problem solving, aesthetic design, and agile workflow. Coding has allowed me to uniquely combine many of my skills and interests in life and I’m excited to share it with the world.</p>
+                <div className='mypic'></div>
+            </div>
+        </div>
     )
 }
