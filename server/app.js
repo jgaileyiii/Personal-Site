e575384ec9c181f486c8a18c03232d40b42b8cb2
Joe Gailey <jgaileyiii@gmail.com>
2020-01-18 17:47:51 -0700

    troubleshooting port issues.

--- server/app.js
+++ server/app.js
@@ -15,10 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors())
-const port = process.env.PORT || 4000
-app.listen(port, function(){
-  console.log(port)
-})
+app.set('port', (process.env.PORT || 4000));
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 app.use('/api/mailer', mailer)
 app.get('/', function(req, res){
