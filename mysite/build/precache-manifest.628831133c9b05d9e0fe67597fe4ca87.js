aaef8c24d0d984e9ab0a328dba6c6d96c109b70b
Joe Gailey <jgaileyiii@gmail.com>
2020-01-18 17:25:13 -0700

    added build folder.

---
+++ mysite/build/precache-manifest.628831133c9b05d9e0fe67597fe4ca87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573c9554ba5653b179b1728e34de1529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db973a74fb6eae820c7",
+    "url": "/static/css/main.e2ec6e7b.chunk.css"
+  },
+  {
+    "revision": "6bc0aa7ed120e55672b4",
+    "url": "/static/js/2.3e6d16be.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3e6d16be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2db973a74fb6eae820c7",
+    "url": "/static/js/main.c67b80c9.chunk.js"
+  },
+  {
+    "revision": "6876d820e132c1f506b4",
+    "url": "/static/js/runtime-main.487d74cb.js"
+  },
+  {
+    "revision": "4c041a0eda877872c3b28c8b2e3c3557",
+    "url": "/static/media/guitar.4c041a0e.png"
+  },
+  {
+    "revision": "7cbc1ce5b056e5458bce7139d5947fa2",
+    "url": "/static/media/mypicture.7cbc1ce5.jpg"
+  }
+]);
