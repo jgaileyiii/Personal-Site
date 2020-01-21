8439a8593325bc6d0ec0e51df211f2098e2cac01
Joe Gailey <jgaileyiii@gmail.com>
2020-01-21 15:39:16 -0700

    removed react build.

--- server/api/mailer.js
+++ server/api/mailer.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const nodemailer = require('nodemailer')
 const credentials = require('./config')
-const cors = require('cors')
 
 const transport = {
     host: 'smtp.gmail.com',
